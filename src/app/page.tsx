1b4bc4c184a6592b2a3d8ff2481d67c837dd3204
Mahmood Alam <Mahmood_Alam@dell.com>
2023-10-17 16:12:15 -0400

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 'use client'
 import Typewriter from 'react-ts-typewriter';
-import hadith from './hadith.json';
+import boosts from './boosts.json';
 
-const randomizedHadith = hadith.sort(() => Math.random() - 0.5);
+const randomizedBoosts = boosts.sort(() => Math.random() - 0.5);
 
 const App = () => {
   return (
     <div className="flex flex-col justify-center h-screen">
-      <h1 className="mx-20 text-center" style={{ fontSize: '2.5rem' }}>
-        The Prophet Muhammad ﷺ said:
-      </h1>
-      <br></br>
-      <h1 className="mx-20 text-center" style={{ fontSize: '1.75rem' }}>
+      <h1 className="mx-20 text-center" style={{ fontSize: '2.75rem' }}>
         <Typewriter 
-          text={randomizedHadith} 
+          text={randomizedBoosts} 
           cursor={false}
           delay={10000}
         />
